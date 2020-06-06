4f5d96aa5aabc7cdc0d16df9344e0687aecea9c8
Manmohan <manmohan881989@gmail.com>
2020-06-06 14:07:07 +0530

    Commmmit the changes

---
+++ Test.java
@@ -0,0 +1,10 @@
+package com.test.mytest;
+
+public class Test 
+{
+    public static void main(String [] args) 
+    {
+       //this is hello java program for jenkin only
+    	System.out.println("Hello Java !! Welcome to Jenkin");
+    }
+}
